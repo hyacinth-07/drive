6f9762b8c31b20b2982dc653322e2b7c8d8e6367
hyacinth-07 <ndr.hya37@gmail.com>
2024-10-09 15:12:52 +0200

    by doing it manually, multer works

--- routes/driveRoutes.js
+++ routes/driveRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const driveController = require('../controllers/controllers');
+// FILE UPLOAD
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 // HELLO WORLD
 router.get('/', driveController.helloWorld);
@@ -13,5 +16,13 @@ router.get('/log-in', (req, res) => res.render('log-in'));
 
 // LOGOUT GET
 router.get('/log-out', driveController.logOut);
+
+///// FILE UPLOAD -- MULTER ////
+
+router.get('/upload', (req, res) => res.render('uploadForm'));
+
+router.post('/upload', upload.single('file'), (req, res) => {
+	res.send(req.body);
+});
 
 module.exports = router;
